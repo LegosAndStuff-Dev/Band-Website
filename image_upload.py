dc355b3e9fd852f29bed94c093cf305847b82ff0
LegosAndStuff-Dev <masonnorberg@midco.net>
2022-11-16 19:28:38 -0600

    added updated code

--- image_upload.py
+++ image_upload.py
@@ -4,6 +4,15 @@ import io
 
 from PIL import Image, ImageFilter, ImageFont, ImageDraw
 
+def my_function(e):
+    text = ""
+
+    for i in range(1, 11):
+        element = f"input-{i}"
+        text += f"{Element(element).element.value}<br>"
+
+    Element("output_format_checker").write(text)
+
 async def _upload_change_and_show(e):
     file_list = e.target.files
     first_item = file_list.item(0)
@@ -16,11 +25,66 @@ async def _upload_change_and_show(e):
     my_image = Image.open(my_bytes)
 
     console.log(f"{my_image.format= } {my_image.width= } {my_image.height= }")
+    W = my_image.width
+    H = my_image.height
 
 
-    font = ImageFont.truetype("Roboto-Black.ttf", 20)
+    font_size = int(H/30)
+    font_size_comp = int(60)
+    font_size_event_type = font_size+5
+    font = ImageFont.truetype("./Roboto-Black.ttf", font_size)
+    fontComp = ImageFont.truetype("./Roboto-Black.ttf", font_size_comp)
+    fontEventType = ImageFont.truetype("./Roboto-Black.ttf", font_size_event_type)
     draw = ImageDraw.Draw(my_image)
-    draw.text((0, 0),"Sample Text",(255,255,255), font=font)
+
+    text = Element("input-comp").element.value
+    w, h = draw.textsize(text, fontComp)
+
+    draw.text(((W-w)-10, 10), text, (255, 255, 255), font=fontComp)
+
+    text = Element("input-type").element.value
+    w, h = draw.textsize(text, fontEventType)
+
+    draw.text(((W-w)-10, 5+10+font_size_comp), text, (255, 255, 255), font=fontEventType)
+
+
+    spotsNotOpen = True
+    i = 0
+
+    while spotsNotOpen:
+        i += 1
+
+        if Element(f"input-{i}").element == None:
+            spotsNotOpen = False
+        else:
+            text = Element(f"input-{i}").element.value
+            w, h = draw.textsize(text, font)
+
+            if "Lincoln" in text:
+                draw.text(((W-w)-10, (20+font_size_comp)+(i*(font_size+5))+25), text, (0,0,0), font=font)
+
+            else:
+                draw.text(((W-w)-10, (20+font_size_comp)+(i*(font_size+5))+25), text,(255,255,255), font=font)
+
+    spotsNotOpen = True
+    i = 0
+
+    while spotsNotOpen:
+        i += 1
+
+        if Element(f"input-{i}-cap").element == None:
+            spotsNotOpen = False
+        else:
+            text = Element(f"input-{i}-cap").element.value
+            console.log(text)
+            w, h = draw.textsize(text, font)
+
+            if "Lincoln" in text:
+                draw.text((10, (20+font_size_comp)+(i*(font_size+5))+25), text, (0,0,0), font=font)
+
+            else:
+                draw.text((10, (20+font_size_comp)+(i*(font_size+5))+25), text,(255,255,255), font=font)
+
 
     my_stream = io.BytesIO()
     my_image.save(my_stream, format="PNG")
@@ -32,6 +96,5 @@ async def _upload_change_and_show(e):
     new_image.src = window.URL.createObjectURL(image_file)
     document.getElementById("output_upload_pillow").appendChild(new_image)
 
-   
 upload_file = create_proxy(_upload_change_and_show)
 document.getElementById("file-upload-pillow").addEventListener("change", upload_file)
